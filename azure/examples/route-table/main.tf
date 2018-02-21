5622e7f81c3fc533711503fbb1d86f8b6db39c76
Paulo Pizarro <paulo.pizarro@neoway.com.br>
2018-02-21 18:07:48 -0300

    Added azure route table example

---
+++ azure/examples/route-table/main.tf
@@ -0,0 +1,45 @@
+
+module "vnet" {
+    source              = "../../modules/vnet"
+    resource_group_name = "test-rg"
+    location            = "eastus"
+    vnet_name           = "test-vnet"
+    address_space       = "10.31.0.0/16"
+    subnet_prefixes     = ["10.31.1.0/24", "10.31.2.0/24"]
+    subnet_names        = ["test-subnet1", "test-subnet2"]
+	route_table_ids     = {
+                            test-subnet1 = "${module.rt1.route_table_id}"
+                            test-subnet2 = "${module.rt2.route_table_id}"
+                          }
+}
+
+module "rt1" {
+    source                     = "../../modules/route-table"
+    resource_group_name        = "test-rg"
+    location                   = "eastus"
+    route_table_name           = "test-rt-1"
+    routes                     = [
+      {
+        name                   = "default1"
+        address_prefix         = "0.0.0.0/0"
+        next_hop_type          = "VirtualAppliance"
+        next_hop_in_ip_address = "10.31.1.100"
+      }
+    ]
+}
+
+module "rt2" {
+    source                     = "../../modules/route-table"
+    resource_group_name        = "test-rg"
+    location                   = "eastus"
+    route_table_name           = "test-rt-2"
+    routes                     = [
+      {
+        name                   = "default2"
+        address_prefix         = "0.0.0.0/0"
+        next_hop_type          = "VirtualAppliance"
+        next_hop_in_ip_address = "10.31.2.100"
+      }
+    ]
+}
+
