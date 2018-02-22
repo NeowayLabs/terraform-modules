5406faf285035f161d90337220a758cd987d98eb
Paulo Pizarro <paulo.pizarro@neoway.com.br>
2018-02-22 12:11:04 -0300

    Defined variables and outputs for azure network module

--- azure/modules/network/outputs.tf
+++ azure/modules/network/outputs.tf
@@ -1,24 +1,35 @@
-output "vnet_id" {
+output "network_vnet_id" {
   description = "The id of the newly created vNet"
-  value       = "${module.terraform-azurerm-vnet.vnet_id}"
+  value       = "${module.vnet.vnet_id}"
 }
 
-output "vnet_name" {
+output "network_vnet_name" {
   description = "The Name of the newly created vNet"
-  value       = "${module.terraform-azurerm-vnet.vnet_name}"
+  value       = "${module.vnet.vnet_name}"
 }
 
-output "vnet_location" {
+output "network_location" {
   description = "The location of the newly created vNet"
-  value       = "${module.terraform-azurerm-vnet.vnet_location}"
+  value       = "${module.vnet.vnet_location}"
 }
 
-output "vnet_address_space" {
+output "network_address_space" {
   description = "The address space of the newly created vNet"
-  value       = "${module.terraform-azurerm-vnet.vnet_address_space}"
+  value       = "${module.vnet.vnet_address_space}"
 }
 
-output "vnet_subnets" {
-  description = "The ids of subnets created inside the newl vNet"
-  value       = "${module.terraform-azurerm-vnet.vnet_subnets}"
+output "network_subnets" {
+  description = "The ids of subnets created inside the newly vNet"
+  value       = "${module.vnet.vnet_subnets}"
 }
+
+output "network_nsgs" {
+  description = "The ids of NSGs created inside the newly vNet"
+  value       = "${modules.nsg.*.network_security_group_id}"
+}
+
+output "network_route_tables" {
+  description = "The ids of route tables created inside the newly vNet"
+  value       = "${modules.route_table.*.route_table_id}"
+}
+
--- azure/modules/network/variables.tf
+++ azure/modules/network/variables.tf
@@ -1,7 +1,4 @@
-variable "vnet_name" {
-  description = "Name of the vnet to create"
-  default     = "acctvnet"
-}
+# Network definition
 
 variable "resource_group_name" {
   description = "Default resource group name that the network will be created in."
@@ -11,6 +8,11 @@ variable "resource_group_name" {
 variable "location" {
   description = "The location/region where the core network will be created. The full list of Azure regions can be found at https://azure.microsoft.com/regions"
   default     = "eastus2"
+}
+
+variable "vnet_name" {
+  description = "Name of the vnet to create"
+  default     = "acctvnet"
 }
 
 variable "address_space" {
@@ -32,6 +34,48 @@ variable "subnet_prefixes" {
 variable "subnet_names" {
   description = "A list of public subnets inside the vNet."
   default     = ["subnet1"]
+}
+
+variable "nsg_names" {
+  description = "A list of network security group names for each subnet inside the vNet."
+  default     = ["nsg1"]
+}
+
+variable "route_table_names" {
+  description = "A list of route table names for each subnet inside the vNet."
+  default     = ["rt1"]
+}
+
+variable "nsg_rules" {
+  description = "A map of network security group rules. The keys are the NSG name and the values are the rules for the respective NSG."
+  default     = {}
+}
+
+variable "route_table_routes" {
+  description = "A map of route table routes. The keys are the route table name and the values are the routes for the respective route table."
+  default     = {}
+}
+
+# Security Rules definition
+
+# Security rules
+# [priority, direction, access, protocol, source_port_range, source_address_prefix, destination_port_range, destination_address_prefix, description]"
+# All the fields are required.
+variable "rules" {
+  description = "Security rules for the network security group using this format name = [priority, direction, access, protocol, source_port_range, source_address_prefix, destination_port_range, destination_address_prefix, description]"
+  type        = "list"
+  default     = []
+}
+
+# Routes definition
+
+# Routes arguments
+# [name, address_prefix, next_hop_type, next_hop_in_ip_address]"
+# All the fields are required excepted next_hop_in_ip_address.
+variable "routes" {
+  description = "Routes for the route table using this format name = [name, address_prefix, next_hop_type, next_hop_in_ip_address]"
+  type        = "list"
+  default     = []
 }
 
 variable "tags" {
