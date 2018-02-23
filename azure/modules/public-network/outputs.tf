2f29ee8eb9ecdd22240a7eba89cb3e140230ebf4
Paulo Pizarro <paulo.pizarro@neoway.com.br>
2018-02-23 11:44:39 -0300

    Fixed output variables for azure pbulic-network module

--- azure/modules/public-network/outputs.tf
+++ azure/modules/public-network/outputs.tf
@@ -1,45 +1,45 @@
 output "vnet_id" {
   description = "The id of the newly created vNet"
-  value       = "${azurerm_virtual_network.vnet.id}"
+  value       = "${module.vnet.vnet_id}"
 }
 
 output "vnet_name" {
   description = "The Name of the newly created vNet"
-  value       = "${azurerm_virtual_network.vnet.name}"
+  value       = "${module.vnet.vnet_name}"
 }
 
 output "vnet_address_space" {
   description = "The address space of the newly created vNet"
-  value       = "${azurerm_virtual_network.vnet.address_space}"
+  value       = "${module.vnet.vnet_address_space}"
 }
 
 output "public_network_location" {
   description = "The location of the public network"
-  value       = "${azurerm_virtual_network.vnet.location}"
+  value       = "${module.vnet.vnet_location}"
 }
 
 output "public_network_subnet_id" {
   description = "The id of public subnet"
-  value       = "${azurerm_subnet.subnet.id}"
+  value       = "${module.subnet.subnet_id}"
 }
 
 output "public_network_subnet_name" {
   description = "The Name of the public subnet"
-  value       = "${azurerm_subnet.subnet.name}"
+  value       = "${module.subnet.subnet_name}"
 }
 
 output "public_network_subnet_prefix" {
   description = "The address prefix for the public subnet"
-  value       = "${azurerm_subnet.subnet.address_prefix}"
+  value       = "${module.subnet.subnet_prefix}"
 }
 
 output "public_network_nsg_id" {
   description = "The id of the public NSG"
-  value       = "${module.nsg.network_security_group_id}"
+  value       = "${module.subnet.subnet_nsg_id}"
 }
 
 output "public_network_route_table_id" {
   description = "The id of the public route table"
-  value       = "${module.route_table.route_table_id}"
+  value       = "${module.subnet.subnet_route_table_id}"
 }
 
