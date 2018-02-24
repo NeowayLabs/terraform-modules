f569bccf8421ac77147155f71754dd309c50e6d8
Paulo Pizarro <paulo.pizarro@neoway.com.br>
2018-02-24 11:48:15 -0300

    Initial version for azure linux virtual machine module

---
+++ azure/modules/linux-vm/variables.tf
@@ -0,0 +1,115 @@
+variable "resource_group_name" {
+  description = "The name of the resource group in which the resources will be created"
+}
+
+variable "location" {
+  description = "The location/region where the virtual network is created. Changing this forces a new resource to be created."
+}
+
+variable "subnet_id" {
+  description = "The subnet id of the virtual network where the virtual machines will reside."
+}
+
+variable "ssh_key" {
+  description = "Path to the public key to be used for ssh access to the VM.  Only used with non-Windows vms and can be left as-is even if using Windows vms. If specifying a path to a certification on a Windows machine to provision a linux vm use the / in the path versus backslash. e.g. c:/home/id_rsa.pub"
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "admin_username" {
+  description = "The admin username of the VM that will be deployed"
+  default     = "bootstrap"
+}
+
+variable "storage_account_type" {
+  description = "Defines the type of storage account to be created. Valid options are Standard_LRS, Standard_ZRS, Standard_GRS, Standard_RAGRS, Premium_LRS."
+  default     = "Premium_LRS"
+}
+
+variable "vm_size" {
+  description = "Specifies the size of the virtual machine."
+  default     = "Standard_DS1_V2"
+}
+
+variable "nb_instances" {
+  description = "Specify the number of vm instances"
+  default     = "1"
+}
+
+variable "vm_hostname" {
+  description = "local name of the VM"
+  default     = "myvm"
+}
+
+variable "vm_os_simple" {
+  description = "Specify UbuntuServer, RHEL, openSUSE-Leap, CentOS, Debian, CoreOS and SLES to get the latest image version of the specified os.  Do not provide this value if a custom value is used for vm_os_publisher, vm_os_offer, and vm_os_sku."
+  default     = "UbuntuServer"
+}
+
+variable "vm_os_id" {
+  description = "The resource ID of the image that you want to deploy if you are using a custom image."
+  default     = ""
+}
+
+variable "vm_os_publisher" {
+  description = "The name of the publisher of the image that you want to deploy. This is ignored when vm_os_id or vm_os_simple are provided."
+  default     = ""
+}
+
+variable "vm_os_offer" {
+  description = "The name of the offer of the image that you want to deploy. This is ignored when vm_os_id or vm_os_simple are provided."
+  default     = ""
+}
+
+variable "vm_os_sku" {
+  description = "The sku of the image that you want to deploy. This is ignored when vm_os_id or vm_os_simple are provided."
+  default     = ""
+}
+
+variable "vm_os_version" {
+  description = "The version of the image that you want to deploy. This is ignored when vm_os_id or vm_os_simple are provided."
+  default     = "latest"
+}
+
+variable "tags" {
+  type        = "map"
+  description = "A map of the tags to use on the resources that are deployed with this module."
+
+  default = {
+  }
+}
+
+variable "private_ip_address_allocation" {
+  description = "Defines how an IP address is assigned. Options are Static or Dynamic."
+  default     = "dynamic"
+}
+
+variable "delete_os_disk_on_termination" {
+  description = "Delete datadisk when machine is terminated"
+  default     = "false"
+}
+
+variable "data_sa_type" {
+  description = "Data Disk Storage Account type"
+  default     = "Standard_LRS"
+}
+
+variable "data_disk_size_gb" {
+  description = "Storage data disk size size"
+  default     = ""
+}
+
+variable "data_disk" {
+  type        = "string"
+  description = "Set to true to add a datadisk."
+  default     = "false"
+}
+
+variable "boot_diagnostics" {
+  description = "(Optional) Enable or Disable boot diagnostics"
+  default     = "false"
+}
+
+variable "boot_diagnostics_sa_type" {
+  description = "(Optional) Storage account type for boot diagnostics"
+  default     = "Standard_LRS"
+}
