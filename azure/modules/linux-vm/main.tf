e0f4f77141821dbf08a0f48a7d958150211a36a1
Paulo Pizarro <paulo.pizarro@neoway.com.br>
2018-02-24 18:20:44 -0300

    Fixed name resource logic

--- azure/modules/linux-vm/main.tf
+++ azure/modules/linux-vm/main.tf
@@ -37,7 +37,7 @@ resource "azurerm_storage_account" "vm-sa" {
 
 resource "azurerm_virtual_machine" "vm-linux" {
   count                         = "${var.data_disk == "false" ? var.nb_instances : 0}"
-  name                          = "${var.vm_hostname}-vm${var.nb_instances == 0 ? "" : "-${count.index}"}"
+  name                          = "${var.vm_hostname}-vm${var.nb_instances == 1 ? "" : "-$${count.index}"}"
   location                      = "${var.location}"
   resource_group_name           = "${azurerm_resource_group.vm.name}"
   availability_set_id           = "${azurerm_availability_set.vm.id}"
@@ -54,14 +54,14 @@ resource "azurerm_virtual_machine" "vm-linux" {
   }
 
   storage_os_disk {
-    name              = "${var.vm_hostname}-vm${var.nb_instances == 0 ? "" : "-${count.index}"}-osdisk"
+    name              = "${var.vm_hostname}-vm${var.nb_instances == 1 ? "" : "-$${count.index}"}-osdisk"
     create_option     = "FromImage"
     caching           = "ReadWrite"
     managed_disk_type = "${var.os_sa_type}"
   }
 
   os_profile {
-    computer_name  = "${var.vm_hostname}${var.nb_instances == 0 ? "" : "-${count.index}"}"
+    computer_name  = "${var.vm_hostname}${var.nb_instances == 1 ? "" : "-$${count.index}"}"
     admin_username = "${var.admin_username}"
   }
 
@@ -84,7 +84,7 @@ resource "azurerm_virtual_machine" "vm-linux" {
 
 resource "azurerm_virtual_machine" "vm-linux-with-datadisk" {
   count                         = "${var.data_disk == "true" ? var.nb_instances : 0}"
-  name                          = "${var.vm_hostname}-vm${var.nb_instances == 0 ? "" : "-${count.index}"}"
+  name                          = "${var.vm_hostname}-vm${var.nb_instances == 1 ? "" : "-$${count.index}"}"
   location                      = "${var.location}"
   resource_group_name           = "${azurerm_resource_group.vm.name}"
   availability_set_id           = "${azurerm_availability_set.vm.id}"
@@ -101,14 +101,14 @@ resource "azurerm_virtual_machine" "vm-linux-with-datadisk" {
   }
 
   storage_os_disk {
-    name              = "${var.vm_hostname}-vm${var.nb_instances == 0 ? "" : "-${count.index}"}-osdisk"
+    name              = "${var.vm_hostname}-vm${var.nb_instances == 1 ? "" : "-$${count.index}"}-osdisk"
     managed_disk_type = "${var.os_sa_type}"
     create_option     = "FromImage"
     caching           = "ReadWrite"
   }
 
   storage_data_disk {
-    name                             = "${var.vm_hostname}-vm${var.nb_instances == 0 ? "" : "-${count.index}"}-datadisk"
+    name                             = "${var.vm_hostname}-vm${var.nb_instances == 1 ? "" : "-$${count.index}"}-datadisk"
     managed_disk_type                = "${var.data_sa_type}"
     create_option                    = "Empty"
     disk_size_gb                     = "${var.data_disk_size_gb}"
@@ -118,7 +118,7 @@ resource "azurerm_virtual_machine" "vm-linux-with-datadisk" {
   }
 
   os_profile {
-    computer_name  = "${var.vm_hostname}${var.nb_instances == 0 ? "" : "-${count.index}"}"
+    computer_name  = "${var.vm_hostname}${var.nb_instances == 1 ? "" : "-$${count.index}"}"
     admin_username = "${var.admin_username}"
   }
 
@@ -150,15 +150,15 @@ resource "azurerm_availability_set" "vm" {
 
 resource "azurerm_network_interface" "vm" {
   count                     = "${var.nb_instances}"
-  name                      = "${var.vm_hostname}-nic${var.nb_instances == 0 ? "" : "-${count.index}"}"
+  name                      = "${var.vm_hostname}-nic${var.nb_instances == 1 ? "" : "-$${count.index}"}"
   location                  = "${azurerm_resource_group.vm.location}"
   resource_group_name       = "${azurerm_resource_group.vm.name}"
   enable_ip_forwarding      = "${var.enable_ip_forwarding}"
 
   ip_configuration {
     name                          = "default-ip-config"
-    subnet_id                     = "${var.vnet_subnet_id}"
-    private_ip_address            = "${element(var.private_ip_address_list,count.index)}"
+    subnet_id                     = "${var.subnet_id}"
     private_ip_address_allocation = "Dynamic"
+    private_ip_address            = "${var.private_ip_address_allocation == "Static" ? element(var.private_ip_address_list,count.index) : "" }"
   }
 }
