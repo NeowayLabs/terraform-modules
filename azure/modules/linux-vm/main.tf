41144639fbcdb56aceaf6525a1db510190531f59
Paulo Pizarro <paulo.pizarro@neoway.com.br>
2018-02-24 20:52:16 -0300

    Fixed private_ip_address_allocation on linux-vm module

--- azure/modules/linux-vm/main.tf
+++ azure/modules/linux-vm/main.tf
@@ -158,7 +158,7 @@ resource "azurerm_network_interface" "vm" {
   ip_configuration {
     name                          = "default-ip-config"
     subnet_id                     = "${var.subnet_id}"
-    private_ip_address_allocation = "Dynamic"
-    private_ip_address            = "${var.private_ip_address_allocation == "Static" ? element(var.private_ip_address_list,count.index) : "" }"
+    private_ip_address_allocation = "${var.private_ip_address_allocation}"
+    private_ip_address            = "${var.private_ip_address_allocation == "static" ? element(var.private_ip_address_list,count.index) : "" }"
   }
 }
