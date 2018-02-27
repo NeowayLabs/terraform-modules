3ba321971db2f9634fc51cc1ea57776454f6d361
Paulo Pizarro <paulo.pizarro@neoway.com.br>
2018-02-27 10:21:17 -0300

    Added initial version for elastic module

---
+++ azure/modules/elastic/outputs.tf
@@ -0,0 +1,106 @@
+
+output "subnet_id" {
+  description = "The id of subnet created inside the vnet"
+  value       = "${module.subnet.subnet_id}"
+}
+
+output "subnet_name" {
+  description = "The Name of the newly created subnet"
+  value       = "${module.subnet.subnet_name}"
+}
+
+output "subnet_prefix" {
+  description = "The address prefix for the newly created subnet"
+  value       = "${module.subnet.subnet_prefix}"
+}
+
+output "subnet_nsg_id" {
+  description = "The id of NSG created and associated to the newly subnet"
+  value       = "${module.nsg.network_security_group_id}"
+}
+
+output "subnet_route_table_id" {
+  description = "The id of route table created and associated to the newly subnet"
+  value       = "${module.route_table.route_table_id}"
+}
+
+output "master_vm_ids" {
+  description = "Master virtual machine ids created."
+  value       = "${module.master.vm_ids}"
+}
+
+output "client_vm_ids" {
+  description = "Client virtual machine ids created."
+  value       = "${module.client.vm_ids}"
+}
+
+output "rack_1_data_vm_ids" {
+  description = "Data virtual machine ids created in rack 1."
+  value       = "${module.rack1-data.vm_ids}"
+}
+
+output "rack_2_data_vm_ids" {
+  description = "Data virtual machine ids created in rack 2."
+  value       = "${module.rack2-data.vm_ids}"
+}
+
+output "master_network_interface_ids" {
+  description = "ids of the master vm nics provisoned."
+  value       = "${module.master.network_interface_ids}"
+}
+
+output "client_network_interface_ids" {
+  description = "ids of the client vm nics provisoned."
+  value       = "${module.client.network_interface_ids}"
+}
+
+output "rack_1_data_network_interface_ids" {
+  description = "ids of the data vm nics provisoned in rack 1."
+  value       = "${module.rack1-data.network_interface_ids}"
+}
+
+output "rack_2_data_network_interface_ids" {
+  description = "ids of the data vm nics provisoned in rack 2."
+  value       = "${module.rack1-data.network_interface_ids}"
+}
+
+output "master_network_interface_private_ip" {
+  description = "private ip addresses of the master vm nics"
+  value       = "${module.master.network_interface_private_ip}"
+}
+
+output "client_network_interface_private_ip" {
+  description = "private ip addresses of the client vm nics"
+  value       = "${module.client.network_interface_private_ip}"
+}
+
+output "rack_1_data_network_interface_private_ip" {
+  description = "private ip addresses of the data vm nics in rack 1"
+  value       = "${module.rack_1_data.network_interface_private_ip}"
+}
+
+output "rack_2_data_network_interface_private_ip" {
+  description = "private ip addresses of the data vm nics in rack 2"
+  value       = "${module.rack_2_data.network_interface_private_ip}"
+}
+
+output "master_availability_set_id" {
+  description = "id of the availability set where the master vms are provisioned."
+  value       = "${module.master.availability_set_id}"
+}
+
+output "client_availability_set_id" {
+  description = "id of the availability set where the client vms are provisioned."
+  value       = "${module.client.availability_set_id}"
+}
+
+output "rack_1_data_availability_set_id" {
+  description = "id of the availability set where the data vms are provisioned in rack 1."
+  value       = "${module.rack_1_data.availability_set_id}"
+}
+
+output "rack_2_data_availability_set_id" {
+  description = "id of the availability set where the data vms are provisioned in rack 2."
+  value       = "${module.rack_2_data.availability_set_id}"
+}
+
---
+++ azure/modules/elastic/variables.tf
@@ -0,0 +1,110 @@
+# Elasticserach variable definition
+
+variable "env" {
+  description = "Environment to orchestrate. This name will be use with a prefix for name of resource group, subnet, nsg, route table, instances"
+  default = "test"
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group in which the resources will be created"
+}
+
+variable "location" {
+  description = "The location/region where the virtual network is created. Changing this forces a new resource to be created."
+  default = "eastus2"
+}
+
+variable "name" {
+  description = "This name will be use with a prefix for name of resource group, subnet, nsg, route table, instances"
+}
+
+variable "vnet_name" {
+  description = "Name of the vnet to associate."
+}
+
+variable "subnet_prefix" {
+  description = "The address prefix to use for the subnet."
+}
+
+variable "security_group_rules" {
+  description = "Security rules for the network security group using this format name = [priority, direction, access, protocol, source_port_range, source_address_prefix, destination_port_range, destination_address_prefix, description]"
+  type = "list"
+  default = []
+}
+
+variable "route_table_routes" {
+  description = "Routes for the route table using this format name = [name, address_prefix, next_hop_type, next_hop_in_ip_address]"
+  type = "list"
+  default = []
+}
+
+variable "admin_username" {
+  description = "The admin username of the VM that will be deployed"
+  default     = "bootstrap"
+}
+
+variable "ssh_key" {
+  description = "Path to the public key to be used for ssh access to the VM.  Only used with non-Windows vms and can be left as-is even if using Windows vms. If specifying a path to a certification on a Windows machine to provision a linux vm use the / in the path versus backslash. e.g. c:/home/id_rsa.pub"
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "master_nb_instances" {
+  description = "Specify the number of instances of master node."
+  default     = "3"
+}
+
+variable "master_vm_size" {
+  description = "Specifies the size of the virtual machine for master node."
+  default     = "Standard_DS2_v2"
+}
+
+variable "master_private_ip_address" {
+  description = "A list of static IP address for each master node."
+  default = [""]
+}
+
+variable "client_nb_instances" {
+  description = "Specify the number of instances of client node."
+  default     = "2"
+}
+
+variable "client_vm_size" {
+  description = "Specifies the size of the virtual machine for client node."
+  default     = "Standard_DS13_v2"
+}
+
+variable "client_private_ip_address" {
+  description = "A list of static IP address for each client node."
+  default = [""]
+}
+
+variable "data_vm_size" {
+  description = "Specifies the size of the virtual machine for data node."
+  default     = "Standard_L8s"
+}
+
+variable "rack_1_data_nb_instances" {
+  description = "Specify the number of instances of data node in rack 1."
+}
+
+variable "rack1_avset_update_domain_count" {
+  description = "Specifies the number of update domains that are used for rack 1."
+}
+
+variable "rack1_avset_fault_domain_count" {
+  description = "Specifies the number of fault domains that are used for rack 1."
+}
+
+variable "rack_2_data_nb_instances" {
+  description = "Specify the number of instances of data node in rack 2."
+  default = "0"
+}
+
+variable "rack2_avset_update_domain_count" {
+  description = "Specifies the number of update domains that are used for rack 2."
+}
+
+variable "rack2_avset_fault_domain_count" {
+  description = "Specifies the number of fault domains that are used for rack 2."
+}
+
